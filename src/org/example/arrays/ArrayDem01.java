4a2c48574573b0cb10917d8474767b112fcb1e01
NegarMadadi <negar.myeah@gmail.com>
2022-04-20 15:50:06 +0200

    Array Demos

---
+++ src/org/example/arrays/ArrayDem01.java
@@ -0,0 +1,17 @@
+package org.example.arrays;
+
+public class ArrayDem01 {
+    public static void main(String[] args) {
+        int[] myIntArray = new int[10];
+        for (int i = 0; i < myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+        printArray(myIntArray);
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
+        }
+    }
+}
