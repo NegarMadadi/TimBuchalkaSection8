b80bfc2275836d1a863352520be5e403b4f9f467
NegarMadadi <negar.myeah@gmail.com>
2022-04-24 22:06:00 +0200

    ReverseArrayChallenge.java is created to reverse a given array.

---
+++ src/org/example/arrays/reverseArrayChallenge/ReverseArrayChallenge.java
@@ -0,0 +1,15 @@
+package org.example.arrays.reverseArrayChallenge;
+
+public class ReverseArrayChallenge {
+
+    public static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+    }
+}
