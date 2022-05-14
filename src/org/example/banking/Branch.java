8ed97ee09054ed6af773ad9bd5f673d068a8eb15
NegarMadadi <negar.myeah@gmail.com>
2022-05-14 18:49:31 +0200

    create Branch class for banking challenge

---
+++ src/org/example/banking/Branch.java
@@ -0,0 +1,67 @@
+package org.example.banking;
+
+//2.Branch
+//
+// It has two fields, A String called name and an ArrayList that holds objects of type Customer called customers.
+//
+// A constructor that takes a String (name of the branch). It initialises name and instantiates customers.
+//
+// And five methods, they are (their functions are in their names):
+//
+// getName(), getter for name.
+//
+// getCustomers(), getter for customers.
+//
+// newCustomer(), has two parameters of type String (name of customer), double (initial transaction) and returns a boolean. Returns true if the customer was added successfully or false otherwise.
+//
+//addCustomerTransaction(), has two parameters of type String (name of customer), double (transaction) and returns a boolean. Returns true if the customers transaction was added successfully or false otherwise.
+//
+//findCustomer(), has one parameter of type String (name of customer) and returns a Customer. Return the Customer if they exist, null otherwise.
+
+
+import java.util.ArrayList;
+
+public class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String customerName, double initialTransaction) {
+        if (findCustomer(customerName) == null) {
+            this.customers.add(new Customer(customerName, initialTransaction));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if (existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName) {
+        for (int i = 0; i < customers.size(); i++) {
+            Customer checkedCustomer = this.customers.get(i);
+            if (checkedCustomer.getName().equals(customerName)) {
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
+}
